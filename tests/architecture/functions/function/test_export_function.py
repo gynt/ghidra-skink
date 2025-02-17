0f457f9b50233ff35786640d1a035a5ea9b8f2e0
Gynt <Gynt@users.noreply.github.com>
2025-02-17 14:15:47 +0100

    test(function): add test_export2 to control how class headers are included

--- tests/architecture/functions/function/test_export_function.py
+++ tests/architecture/functions/function/test_export_function.py
@@ -1,7 +1,6 @@
 from skink.export.context import DEFAULT
 from skink.architecture.functions.function import Function
 
-# TODO: A/B.h is aspecific, should be with optional suffix Class
 def test_export1(example_class_example_functions_fixture):
     func = example_class_example_functions_fixture[0]
     ff = Function(func)
@@ -15,6 +14,24 @@ def test_export1(example_class_example_functions_fixture):
 namespace A::B {
 
   BOOL __thiscall a(BClass * this, undefined4 x, undefined4 y);
+
+}\
+"""
+
+# TODO: this looks like a circular import to me!
+def test_export2(example_class_example_functions_fixture):
+    func = example_class_example_functions_fixture[0]
+    ff = Function(func)
+    ctx = DEFAULT.mutate(promote_to_class = False, include = DEFAULT.include.mutate(functions_this_parameter_type = True))
+    e = ff.export(ctx)
+    assert e == \
+"""\
+#include "A/B.h"
+#include "WinDef.h"
+
+namespace A::B {
+
+  BOOL __thiscall a(B * this, undefined4 x, undefined4 y);
 
 }\
 """
